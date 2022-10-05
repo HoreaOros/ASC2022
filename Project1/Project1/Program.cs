604af28e7ab1c53adc9c48f61ee96e9039c723e3
Horea Oros <horea.oros@gmail.com>
2022-10-05 13:43:41 +0300

    Add rounding error example

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -10,7 +10,13 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-
+            float eps = 0.001f;
+            float suma = 0f;
+            for (int i = 0; i < 1000000; i++)
+            {
+                suma += eps;
+            }
+            Console.WriteLine(suma);
         }
     }
 }
