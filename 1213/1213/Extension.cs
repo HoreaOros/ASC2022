1c3fccdb12c9c79a2d8cd3420bd8bd1c3978b82a
Horea Oros <horea.oros@gmail.com>
2022-12-13 09:51:46 +0200

    add 13 dec project - BitOperators - lab

---
+++ 1213/1213/Extension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1213
+{
+    public static class Extension
+    {
+        /*
+         * 00110101
+         * 00001000  OR
+         * --------   
+         * 00111101
+         */
+        public static uint SetBit(this uint n, int offset)
+        {
+            uint mask;
+            mask = 1u << offset;
+            n |= mask;
+            return n;
+        }
+        /*
+         * 00111101
+         * 11110111 AND
+         * --------
+         * 00110101
+         */
+        public static uint ResetBit(this uint n, int offset)
+        {
+            uint mask;
+            mask = ~(1u << offset);
+            n &= mask;
+            return n;
+        }
+        /*
+         * 00111101
+         * 00000111
+         * 00000001 AND
+         * ---------
+         */
+        public static uint GetBit(this uint n, int offset)
+        {
+            return (n >> offset) & 1u;
+        }
+    }
+}
---
+++ 1213/1213/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1213
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = 0b1001_0101_1100_0000_0101;
+            Console.WriteLine($"{Convert.ToString(a, toBase: 2).PadLeft(32, '0'), 32}");
+            Console.WriteLine($"{a.GetBit(3)}");
+
+            a = a.SetBit(3);
+            Console.WriteLine($"{Convert.ToString(a, toBase: 2).PadLeft(32, '0'),32}");
+            Console.WriteLine($"{a.GetBit(3)}");
+
+            a = a.ResetBit(3);
+            Console.WriteLine($"{Convert.ToString(a, toBase: 2).PadLeft(32, '0'),32}");
+            Console.WriteLine($"{a.GetBit(3)}");
+
+
+            uint m = 2587, k = 3658, c, r;
+            c = m ^ k;
+            r = c ^ k;
+            Console.WriteLine($"r = {r}");
+        }
+    }
+}
